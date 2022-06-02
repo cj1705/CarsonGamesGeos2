81ab1a4a0518141c7b2b08a7a3b87e444944d8bf
Carson Games (Laptop) <Carson@DESKTOP-8K4ENNK>
2022-06-02 10:17:48 -0400

    added a boarder around the background color selection

--- CarsonGamesGeos2/MainForms/Misc Forms/fts/forms/UserSetup.Designer.cs
+++ CarsonGamesGeos2/MainForms/Misc Forms/fts/forms/UserSetup.Designer.cs
@@ -170,6 +170,7 @@
             // 
             // panel1
             // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Location = new System.Drawing.Point(214, 213);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(23, 19);
