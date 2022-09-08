f6f6f5ba131f99ee63fac0003e71eb8cf2b5384d
Carson Games (Laptop) <Carson@DESKTOP-8K4ENNK>
2022-09-08 13:18:15 -0400

    .

--- CarsonGamesGeos2/classes/GitHubProject.cs
+++ CarsonGamesGeos2/classes/GitHubProject.cs
@@ -9,13 +9,12 @@ namespace CarsonGamesGeos2.classes
 {
     public class GitHubProject
     {
-        //public async GitHubClient getProject()
-        //{
-        //    GitHubClient project = new GitHubClient(new ProductHeaderValue("CarsonGamesGeos2"));
-        //    var user = await project.User.Get("cj1705");
-        //var commits =  await project.Repository.Commit.GetAll("cj1705", "CarsonGamesGeos2");
-        //    var lastest = commits[0];
-            
-        //}
+        public async Task<GitHubClient> getProject()
+        {
+            var client = new GitHubClient(new ProductHeaderValue("CarsonGamesGeos2"));
+            var user = await client.User.Get("cj1705");
+            return client;
+
+        }
     }
 }
--- CarsonGamesGeos2/classes/updates/check.cs
+++ CarsonGamesGeos2/classes/updates/check.cs
@@ -1,15 +1,22 @@
-﻿using System;
+﻿using Octokit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CarsonGamesGeos2.classes.updates
+    
 {
     public class check
     {
-        public bool IsUpdates()
+        GitHubProject GitHubProject = new GitHubProject();
+        
+        public async Task<bool> IsUpdates()
         {
+            var project = await GitHubProject.getProject();
+            
+            
             return true;
         }
     }
